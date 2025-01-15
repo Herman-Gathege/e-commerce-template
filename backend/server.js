1e48488e1d80b46daa6063ee8a7c96a1904d9a90
Herman-Gathege <172432295+Herman-Gathege@users.noreply.github.com>
2025-01-15 13:37:22 +0300

    Fix: Resolve path module issue and define __dirname for ES modules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 
 
 import authRoutes from "./routes/auth.route.js"
